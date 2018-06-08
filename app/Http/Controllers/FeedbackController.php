04457a4b1564492c77972c90f207286f1086d95a
sarvpriywinklix <39791849+aryaWinklix@users.noreply.github.com>
2018-06-08 17:27:34 +0530

    Add files via upload

---
+++ app/Http/Controllers/FeedbackController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Feedback;
+use App\Item;
+
+class FeedbackController extends Controller
+{
+    public function getFeedbacks()
+    {
+    	$feedbacks = Feedback::all();
+    	$items = Item::all();
+    	$finalarray = array();
+    	foreach ($items as $item) {
+    		foreach ($feedbacks as $feedback) {
+    			if(in_array($item->id, unserialize($feedback->selected_snacks)))
+    			{
+    				$item_name =Item::where('id',$item->id)->first()->name;
+    				// echo $item_name;
+    				// exit();
+    				if(array_key_exists($item_name, $finalarray))
+    				{
+
+    					$finalarray[$item_name]=$finalarray[$item_name]+1;
+    				}
+    				else
+    				{
+    				$finalarray[$item_name]=1;
+    			}
+    			}
+    		}
+    	}
+    	$finalarray['totalFeedbacks']=count($feedbacks);
+    	//echo count($feedbacks);
+    	// echo "<pre>";
+    	// 		print_r($finalarray);
+    	// exit();
+        return view('admin.feedback.index')->with('feedbacks',$finalarray);
+    }
+}
