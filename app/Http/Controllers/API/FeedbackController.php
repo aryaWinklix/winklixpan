6d393f1f267e8505de24179ee284fa6b24ab0d89
sarvpriywinklix <39791849+aryaWinklix@users.noreply.github.com>
2018-06-08 16:35:01 +0530

    Add files via upload

---
+++ app/Http/Controllers/API/FeedbackController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Feedback;
+class FeedbackController extends Controller
+{
+    public function addFeedback(Request $request)
+    {
+            //return $request;
+    	//return $ABC = Feedback::where('id',7)->first();
+            $feedback = new Feedback;
+            $feedback->user_id = $request->user_id;
+            $feedback->selected_snacks = serialize($request->selected_snacks);
+            $feedback->how_often = $request->how_often;
+            $feedback->comments = $request->comments;
+            // $abc = unserialize($feedback->selected_snacks);
+            // echo $abc[0];
+            // exit();
+            if ($feedback->save()) {
+                 //return "true";
+                $res = [
+                    'status' => 'success',
+                    'message' => 'Feedback Added Successfully',
+                    // 'feedbackDetails' => $feedback,
+                ];
+                return $res;
+            }else{
+                // return "false";
+                $res = [
+                    'status' => 'failure',
+                    'message' => 'feedback Not Added',
+                ];
+                return $res;
+            }
+         
+
+    }
+}
